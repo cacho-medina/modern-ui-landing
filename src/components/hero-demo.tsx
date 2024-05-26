c3049a930aeec2783876375cb4c55149b7807211
Joaquin Medina <zanlorenzocacho10@gmail.com>
2024-05-26 04:40:39 -0300

    primer diseño

---
+++ src/components/hero-demo.tsx
@@ -0,0 +1,93 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "./ui/hero-parallax";
+
+export function HeroParallaxDemo() {
+    return <HeroParallax products={products} />;
+}
+export const products = [
+    {
+        title: "Blog de Recetas",
+        link: "https://blog-de-recetas.vercel.app/",
+        thumbnail: "blog-recetas.png",
+    },
+    {
+        title: "Boulevard Sur",
+        link: "https://boulevard-sur.vercel.app",
+        thumbnail: "/boulevard.png",
+    },
+
+    {
+        title: "SSIT Group",
+        link: "https://ssit-group.vercel.app",
+        thumbnail: "/ssit.png",
+    },
+    {
+        title: "La Pionera Yerba",
+        link: "https://www.lapionerayerba.com",
+        thumbnail: "pionera.png",
+    },
+    {
+        title: "Rolling Hotel",
+        link: "https://rolling-hotel.vercel.app/",
+        thumbnail: "rolling-hotel.png",
+    },
+
+    {
+        title: "Hostal Las Carretas",
+        link: "https://lascarretashostal.com/",
+        thumbnail: "las-carretas.png",
+    },
+    {
+        title: "Blog de Recetas",
+        link: "https://blog-de-recetas.vercel.app/",
+        thumbnail: "blog-recetas.png",
+    },
+    {
+        title: "Boulevard Sur",
+        link: "https://boulevard-sur.vercel.app",
+        thumbnail: "/boulevard.png",
+    },
+
+    {
+        title: "SSIT Group",
+        link: "https://ssit-group.vercel.app",
+        thumbnail: "/ssit.png",
+    },
+    {
+        title: "La Pionera Yerba",
+        link: "https://www.lapionerayerba.com",
+        thumbnail: "pionera.png",
+    },
+    {
+        title: "Rolling Hotel",
+        link: "https://rolling-hotel.vercel.app/",
+        thumbnail: "rolling-hotel.png",
+    },
+
+    {
+        title: "Hostal Las Carretas",
+        link: "https://lascarretashostal.com/",
+        thumbnail: "las-carretas.png",
+    },
+    {
+        title: "SSIT Group",
+        link: "https://ssit-group.vercel.app",
+        thumbnail: "/ssit.png",
+    },
+    {
+        title: "La Pionera Yerba",
+        link: "https://www.lapionerayerba.com",
+        thumbnail: "pionera.png",
+    },
+    {
+        title: "Boulevard Sur",
+        link: "https://boulevard-sur.vercel.app",
+        thumbnail: "/boulevard.png",
+    },
+    {
+        title: "Hostal Las Carretas",
+        link: "https://lascarretashostal.com/",
+        thumbnail: "las-carretas.png",
+    },
+];
